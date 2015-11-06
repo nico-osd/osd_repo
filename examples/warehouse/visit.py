4b73c6e14513e9dfa6269a4b179451f3be1598ab
HenN <wi13b091@technikum-wien.at>
2015-11-06 17:48:41 +0100

    moved files to nice folders

--- examples/warehouse/visit.py
+++ examples/warehouse/visit.py
@@ -2,9 +2,11 @@
 
 # This is the code that visits the warehouse.
 import sys
+
 import Pyro4
 import Pyro4.util
-from person import Person
+
+from examples.warehouse.person import Person
 
 sys.excepthook = Pyro4.util.excepthook
 
--- examples/warehouse/warehouse.py
+++ examples/warehouse/warehouse.py
@@ -2,8 +2,8 @@
 
 
 from __future__ import print_function
+
 import Pyro4
-import person
 
 
 class Warehouse(object):
