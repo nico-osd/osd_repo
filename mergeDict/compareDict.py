7ddcfab9c91530d507d2bb043d661a1b0d9f398b
bernhard streiter <wi13b012@technikum-wien.at>
2015-12-05 16:54:55 +0100

    das ist vorerst nur prozedural - in createDict werden 2 dictionaries erstellt (hardcoded) - dann gemerged (keys sind per definition unique), dann werden timestamps mit einer referenz verglichen, alles was älter als 5 minuten ist, wird vom dict rausgelöscht.
    next steps: das alles objektorientiert abhandeln.

--- mergeDict/compareDict.py
+++ mergeDict/compareDict.py
@@ -1,5 +1,9 @@
 #!/bin/python3
 
+class twoDicts(object):
 
+    def compareDict(self, dict1, dict2):
+        pass
 
-
+    def getMergedDict(self):
+        pass
--- mergeDict/myDict.py
+++ mergeDict/myDict.py
@@ -6,21 +6,18 @@
 import time
 import datetime
 
-#ts = time.time()
-#print(ts)
-#st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
 class MyList(object):
-      def __init__(self):
-          pass
+    def __init__(self):
+        pass
 
-      def create_liste(self):
-          pass
+    def create_liste(self):
+        pass
 
-      def add_item(self,dict):
-          pass
+    def add_item(self,dict):
+        pass
 
-
+    def get_item(self):
+        pass
 
 
 
