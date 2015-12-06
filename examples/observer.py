7f1f94d70c12ad5aba80bcb6451d53fdec7c33a8
HenN <wi13b091@technikum-wien.at>
2015-12-06 20:00:04 +0100

    observer pattern python'ed
    this is related to #5 - added ABC support in respect to PEP3119. Please write method names with respect to PEP8 and document the methods with respect to PEP257. Deleted parentheses and semicolons.

--- examples/observer.py
+++ examples/observer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-'''Observer pattern:
+"""Observer pattern:
 
 Observer defines a one-to-many dependency between objects
 so that when one object changes state, all its dependents
@@ -8,88 +8,107 @@ are notified and updated automatically.
 
 Provides an observable main class that notifies observers
 or listener of changes happening in the observed class.
-'''
+"""
+from abc import ABCMeta, abstractmethod
 
-class ObservableInterface():
-    '''Observer pattern: Observable interface'''
-    
+
+class ObservableInterface(metaclass=ABCMeta):
+    """Observer pattern: Observable interface"""
+
+    @abstractmethod
     def __init__(self):
         pass;
-    
+
+    @abstractmethod
     def addObserver(self, observer):
         pass;
-    
+
+    @abstractmethod
     def removeObserver(self, observer):
         pass;
-    
+
+    @abstractmethod
     def notifyObservers(self):
         pass;
-#end class
 
-import random;                  # required to update _privateData
+
+# end class
+
+import random  # required to update _privateData
+
 
 class ObservableImplementation(ObservableInterface):
-    '''Observer pattern: Implementation class for Observable'''
-    
-    _observers = [];            # list of observers
-    _privateData = 1;           # private data
-    
+    """Observer pattern: Implementation class for Observable"""
+
+    _observers = []  # list of observers
+    _privateData = 1  # private data
+
     def __init__(self):
-        _observers = [];
-    
+        super().__init__()
+        _observers = []
+
     def addObserver(self, observer):
-        self._observers.append(observer);
-        print ("registered observer %s", observer)
-    
+        self._observers.append(observer)
+        print("registered observer %s", observer)
+
     def removeObserver(self, observer):
-        self._observers.remove(observer);
-    
+        self._observers.remove(observer)
+
     def notifyObservers(self):
         for x in self._observers:
-            x.update(self._privateData);
-    
-    def updatePrivateData(self):
-        _privateData = random.randint(0, 10);
-        self.notifyObservers();
-#end class
+            x.update(self._privateData)
 
-class ObserverInterface():
-    '''Observer pattern: Observer interface'''
-    
+    def updatePrivateData(self):
+        _privateData = random.randint(0, 10)
+        self.notifyObservers()
+
+
+# end class
+
+class ObserverInterface(metaclass=ABCMeta):
+    """Observer pattern: Observer interface"""
+
+    @abstractmethod
     def __init__(self):
         pass;
-    
-    def update(self):
+
+    @abstractmethod
+    def update(self, data):
         pass;
-#end class
+
+
+# end class
 
 class Observer(ObserverInterface):
-    '''Observer pattern: Implementation class for Observer'''
-    
-    _observed = '';
-    
-    def __init__(self, observed = ''):
-        self._observed = observed;
-        print(self);
-    
+    """Observer pattern: Implementation class for Observer"""
+
+    _observed = ''
+
+    def __init__(self, observed=''):
+        super().__init__()
+        self._observed = observed
+        print(self)
+
     def subscribeObserved(self, observed):
-        observed.addObserver(self);
-    
+        observed.addObserver(self)
+
     def unsubscribeObserved(self, observed):
-        observed.removeObserver(self);
-    
+        observed.removeObserver(self)
+
     def update(self, data):
         # TODO: implement update function
-        print("Me: ", self, " Data: ", data);
-#end class
+        print("Me: ", self, " Data: ", data)
 
-if (__name__ == '__main__'):
-    subject = ObservableImplementation();
-    obs1 = Observer();
-    obs2 = Observer();
-    subject.addObserver(obs1);
-    subject.updatePrivateData();
-    subject.addObserver(obs2);
-    subject.updatePrivateData();
-    subject.removeObserver(obs1);
-    subject.updatePrivateData();
+
+# end class
+
+if __name__ == '__main__':
+    subject = ObservableImplementation()
+    obs1 = Observer()
+    obs2 = Observer()
+    subject.addObserver(obs1)
+    subject.updatePrivateData()
+    subject.addObserver(obs2)
+    subject.updatePrivateData()
+    subject.removeObserver(obs1)
+    subject.updatePrivateData()
