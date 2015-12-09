b796548ee2995fe30673136be9eab088304ca4f2
Nico <wi13b091@technikum-wien.at>
2015-12-09 11:08:44 +0100

    related to #8
    implemented a method which returns the ip-address assigned to a network interface. The parameter specifies the name of the interface.
    
    props go to:
    http://code.activestate.com/recipes/439094-get-the-ip-address-associated-with-a-network-inter/

---
+++ util/networking.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+"""
+From: http://code.activestate.com/recipes/439094-get-the-ip-address-associated-with-a-network-inter/
+
+Implements a function, which returns the ip-address assigned to the network interface identified by the name given.
+"""
+
+import fcntl
+import socket
+import struct
+
+
+def get_ip_address(ifname):
+    """
+    shall return the ip address of the given interface-name
+    :param ifname: the name of the network interface
+    :return: ip address assigned to the network interface
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+    # usage:
+    # get_ip_address('lo')
+    # returns: '127.0.0.1'
+    # get_ip_address('eth0')
+    # returns: '38.113.228.130'
